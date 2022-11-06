882caa39e6c8a7d696087f75360a6a0c11049c19
Jackson Brouwer <jacksonbrouwer@MacBook-Pro-69.local>
2022-11-06 16:56:55 -0600

    Entire tutorial

---
+++ laragigstutorial/app/Http/Controllers/ListingController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    // show all listings 
+    public function index() {
+        return view('listings.index', [
+            'heading' => 'Latest Listings', 
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
+        ]);
+    }
+
+    // Show single listing 
+    public function show(Listing $listing) {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+
+    // Show create form
+    public function create() {
+        return view('listings.create');
+    }
+
+    // Store listing data
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'title' => 'required', 
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required', 
+            'website' => 'required', 
+            'email' => ['required', 'email'], 
+            'tags' => 'required', 
+            'description' => 'required'
+
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $formFields['user_id'] = auth()->id(); 
+
+        Listing::create($formFields); 
+
+        // Session::flash('message', "Listing Created"); 
+
+        return redirect('/')->with('message', 'Listing created succesfully!');
+    }
+
+    // Show Edit Form
+    public function edit(Listing $listing) {
+        return view('listings.edit', ['listing' => $listing]); 
+    }
+
+    // Update Listing 
+    public function update(Request $request, Listing $listing) {
+        // Make sure logged in user is owner 
+        if($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized action'); 
+        } 
+
+        $formFields = $request->validate([
+            'title' => 'required', 
+            'company' => 'required',
+            'location' => 'required', 
+            'website' => 'required', 
+            'email' => ['required', 'email'], 
+            'tags' => 'required', 
+            'description' => 'required'
+
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields); 
+
+        // Session::flash('message', "Listing Created"); 
+
+        return back()->with('message', 'Listing created succesfully!');
+    }
+
+    // Delete Listing
+    public function destroy(Listing $listing) {
+        // Make sure logged in user is owner 
+        if($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized action'); 
+        } 
+        
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing successfully deleted');
+    }
+
+    // Manage Listing
+    public function manage() {
+        return view('listings.manage', ['listings' => auth()->user()->listings()->get()]); 
+    }
+}
---
+++ laragigstutorial/routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
+
+// All listings
+Route::get('/', [ListingController::class, 'index']);
+
+// Store listing data
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+
+// Show Create Form 
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+// Edit Submit to Update
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+// Manage Listings
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth'); 
+
+// Single listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register/Create Form
+Route::get('register', [UserController::class, 'create'])->middleware('guest');
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+// Log out User
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest'); 
+
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+
